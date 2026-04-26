c7d49ea6e2e1ac5dcdf78c164a48c3f6a2e652d9
angelo <angelostefanini04@gmail.com>
2026-04-26 19:31:53 +0200

    Remove unnecessary newline in docstring of traj_generation.py

--- src/traj_generation.py
+++ src/traj_generation.py
@@ -13,7 +13,7 @@ Pipeline:
 # NOTE: Cubic Splines might be overkill, a much easier approach would be that of using straight lines in joint space with a trapezoidal velocity profile. 
 We will have to test this out and then consider switching to a simpler approach if the cubic spline interpolation is not satisfactory.
 """
-
+#
 
 from __future__ import annotations
 from pathlib import Path
